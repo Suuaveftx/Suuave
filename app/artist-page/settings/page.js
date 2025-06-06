57e117bfd0deab8baa4ff24641d2bc8d9989d71e
aorfdesigns <rahformatives@gmail.com>
2025-06-06 23:54:24 +0100

    my third settings commit

--- app/artist-page/settings/page.js
+++ app/artist-page/settings/page.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import SettingsCard from './_components/SettingsCard'
+import SettingsCard from './_components/Settings-Page'
 import SettingsCard2 from './_components/SettingsCard2'
+import SettingsPage from './_components/Settings-Page'
 
 const Page = () => {
   return (
     <div className='mt-16 ml-4 flex gap-4 h-auto'>
-        <SettingsCard />
-        <SettingsCard2 />
+        <SettingsPage />
+        <SettingsCard2/>
     </div>
   )
 }
