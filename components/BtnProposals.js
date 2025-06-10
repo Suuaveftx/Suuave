a3e5e15cc47b0af7f919c55aa687d43bb086e3de
aorfdesigns <rahformatives@gmail.com>
2025-06-10 15:10:46 +0100

    Added: Added a bg-radial colour to the Custom button in the BtnProposals component

--- components/BtnProposals.js
+++ components/BtnProposals.js
@@ -12,26 +12,25 @@ const BtnProposals = () => {
           <div>
             <CustomButton
               text="Send Proposal"
-              className="w-48"
+              className="w-48 bg-[radial-gradient(circle,#FFFFFF,#CCE7F2)] text-[#035A7A]"
               href="/artist-page/send-proposal"
             />
           </div>
-          <div>
-            <CustomButton
-  className="w-48 !bg-transparent border border-[#3A98BB] text-[#767676]  flex items-center justify-center gap-2"
-  text="Save Job"
->
-  <Image
-    src="/dev-images/Bookmark.png" // Replace with your actual image path
-    alt="Bookmark icon"
-    className="w-5 h-5"
-    width={20}
-    height={20}
-  />
-  <span>Save Job</span>
-</CustomButton>
+       <div>
+  <CustomButton
+    className="w-48 !bg-transparent border border-[#3A98BB] text-[#767676] flex items-center justify-center gap-2"
+  >
+    <Image
+      src="/dev-images/Bookmark.png"
+      alt="Bookmark icon"
+      className="w-5 h-5"
+      width={20}
+      height={20}
+    />
+    <span>Save Job</span>
+  </CustomButton>
+</div>
 
-          </div>
         </div>
       </CardBody>
     </Card>
