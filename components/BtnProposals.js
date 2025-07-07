93bb75280aff55ecb29539dff2135a24caaca27f
aorfdesigns <rahformatives@gmail.com>
2025-07-08 00:48:34 +0100

    Removed the card component in the Btn proposal and worked on the desktop and mobile view

--- components/BtnProposals.js
+++ components/BtnProposals.js
@@ -2,14 +2,13 @@
 import React from "react";
 import { Card, CardBody } from "@heroui/react";
 import CustomButton from "./CustomButton";
-import Image from "next/image";
+import { MdOutlineBookmarkBorder } from "react-icons/md";
 
 const BtnProposals = () => {
   return (
-    <Card className="py-4 w-64">
-      <CardBody className="overflow-visible py-2">
-        <div className="flex flex-col gap-4 justify-center items-center">
-          <div>
+    
+        <div className="flex lg:flex-col lg:gap-6 gap-4 justify-center items-center lg:bg-[#FAFAFA] bg-[#FFFFFF] px-4 py-4 lg:px-6 lg:py-8 lg:w-screen lg:max-w-[80%] w-screen max-w-[100%] border-1 border-[#EAEAEA] lg:border-1 lg:border-[#EAEAEA] lg:rounded-2xl">
+          <div >
             <CustomButton
               text="Send Proposal"
               className="w-48 bg-[radial-gradient(circle,#FFFFFF,#CCE7F2)] text-[#035A7A]"
@@ -20,22 +19,17 @@ const BtnProposals = () => {
        <div>
   <CustomButton
     className="w-48 !bg-transparent border border-[#3A98BB] text-[#767676] flex items-center justify-center gap-2"
+    icon={<MdOutlineBookmarkBorder className="w-5 h-5" color="#3A98BB" />}
+    text="Save Job"
   >
-    <Image
-      src="/dev-images/Bookmark.png"
-      alt="Bookmark icon"
-      className="w-5 h-5"
-      width={20}
-      height={20}
-    />
-    <span>Save Job</span>
+    
+    
   </CustomButton>
 </div>
 
 
         </div>
-      </CardBody>
-    </Card>
+  
   );
 };
 
