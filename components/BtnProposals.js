c72649598b55b04ce1a49d75c4e20d9401e65314
aorfdesigns <rahformatives@gmail.com>
2025-07-08 13:31:01 +0100

    Added the transparent bg style to the Save job custom button in the Btn proposal file

--- components/BtnProposals.js
+++ components/BtnProposals.js
@@ -11,16 +11,21 @@ const BtnProposals = () => {
           <div >
             <CustomButton
               text="Send Proposal"
-              className="w-48 bg-[radial-gradient(circle,#FFFFFF,#CCE7F2)] text-[#035A7A]"
+              className="w-48  text-[#035A7A]"
               href="/artist-page/send-proposal"
             />
           </div>
 
        <div>
   <CustomButton
-    className="w-48 !bg-transparent border border-[#3A98BB] text-[#767676] flex items-center justify-center gap-2"
+    className="w-48   text-[#767676] flex items-center justify-center gap-2"
     icon={<MdOutlineBookmarkBorder className="w-5 h-5" color="#3A98BB" />}
     text="Save Job"
+    style={{
+      background: "transparent",
+      color: "#767676",
+      border: "1px solid #3A98BB",
+    }}
   >
     
     
