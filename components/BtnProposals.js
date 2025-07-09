80030729d2641ea344c90bbb88951e323ba6e103
aorfdesigns <rahformatives@gmail.com>
2025-07-09 18:02:00 +0100

    Changed the Btn Proposals component into a reusable component file

--- components/BtnProposals.js
+++ components/BtnProposals.js
@@ -1,40 +1,42 @@
 "use client";
 import React from "react";
-import { Card, CardBody } from "@heroui/react";
 import CustomButton from "./CustomButton";
 import { MdOutlineBookmarkBorder } from "react-icons/md";
 
-const BtnProposals = () => {
+const  BtnProposals = ({
+  sendText = "Send Proposal",
+  saveText = "Save Job",
+  sendHref = "/artist-page/send-proposal",
+  saveIcon = <MdOutlineBookmarkBorder className="w-5 h-5" color="#3A98BB" />,
+  saveButtonStyle = {},
+  containerClassName = "",
+}) => {
   return (
-    
-        <div className="flex lg:flex-col lg:gap-6 gap-4 justify-center items-center lg:bg-[#FAFAFA] bg-[#FFFFFF] px-4 py-4 lg:px-6 lg:py-8 lg:w-screen lg:max-w-[80%] w-screen max-w-[100%] border-1 border-[#EAEAEA] lg:border-1 lg:border-[#EAEAEA] lg:rounded-2xl">
-          <div >
-            <CustomButton
-              text="Send Proposal"
-              className="w-48  text-[#035A7A]"
-              href="/artist-page/send-proposal"
-            />
-          </div>
+    <div
+      className={`flex lg:flex-col lg:gap-6 gap-4 justify-center items-center lg:bg-[#FAFAFA] bg-[#FFFFFF] px-4 py-4 lg:px-6 lg:py-8 lg:w-screen lg:max-w-[80%] w-screen max-w-[100%] border border-[#EAEAEA] lg:rounded-2xl ${containerClassName}`}
+    >
+      <div>
+        <CustomButton
+          text={sendText}
+          className="w-48 text-[#035A7A]"
+          href={sendHref}
+        />
+      </div>
 
-       <div>
-  <CustomButton
-    className="w-48   text-[#767676] flex items-center justify-center gap-2"
-    icon={<MdOutlineBookmarkBorder className="w-5 h-5" color="#3A98BB" />}
-    text="Save Job"
-    style={{
-      background: "transparent",
-      color: "#767676",
-      border: "1px solid #3A98BB",
-    }}
-  >
-    
-    
-  </CustomButton>
-</div>
-
-
-        </div>
-  
+      <div>
+        <CustomButton
+          className="w-48 text-[#767676] flex items-center justify-center gap-2"
+          icon={saveIcon}
+          text={saveText}
+          style={{
+            background: "transparent",
+            color: "#767676",
+            border: "1px solid #3A98BB",
+            ...saveButtonStyle,
+          }}
+        />
+      </div>
+    </div>
   );
 };
 
