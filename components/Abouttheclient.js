6388a7149e1f43db5cde2eeed197aeffc95480fb
aorfdesigns <rahformatives@gmail.com>
2025-07-09 16:53:20 +0100

    Redesigned the About the client comonent file

--- components/Abouttheclient.js
+++ components/Abouttheclient.js
@@ -1,57 +1,75 @@
 'use client'
 import React from 'react'
-import {Card,  CardBody, } from "@heroui/react";
-import Image from 'next/image';
+import { MdLocationPin } from "react-icons/md";
+import { FaStar } from "react-icons/fa6";
+import { FaRegUser } from "react-icons/fa6";
+import { MdOutlineMailOutline } from "react-icons/md";
+import { MdOutlinePhone } from "react-icons/md";
+import { LuCreditCard } from "react-icons/lu";
 const Abouttheclient = () => {
   return (
-    <Card className="py-4 w-64">
-    <CardBody className="overflow-visible py-2">
-      <div className='flex flex-col gap-4'>
+  <div className='bg-[#FAFAFA] lg:w-screen lg:max-w-[80%] lg:px-6 px-4 pt-0 pb-[56px] w-screen max-w-96 lg:mx-0 mx-4 rounded-2xl'>
+    <div className='lg:mb-8 mb-6 px-0 py-4'>
+       <h4 className='border-b lg:text-[22px] text-[18px] lg:whitespace-normal whitespace-nowrap'>About the Client</h4> 
+    </div>
+    <div className='mb-8'>
+        
+    <div className='flex flex-col gap-2 mb-6'>
     <div>
-    <h4 className='font-bold text-[#222222]'>About the Client</h4>
-    <hr className='mt-2'></hr>
+        <h5>Fashion Brand</h5>
     </div>
-    <div className='mt-2'>
-        <h6 className='font-bold text-[#222222]'>Fashion Brand</h6>
-        <div className='flex gap-2 mt-2 items-center'>
-            <Image src={"/dev-images/location.png"} width={20} height={20} alt='location' />
-            <p className='text-sm'>Lagos, Nigeria</p>
+    <div className='flex items-center gap-2'>
+<MdLocationPin color='#878787' />
+<span className='tracking-[0.33px] text-[#767676]'>Lagos, Nigeria</span>
+    </div>
+     <div className='flex items-center gap-2'>
+        <span className='tracking-[0.33px] text-[#767676]'>Ratings</span>
+<div className="flex">
+      {Array(5).fill(null).map((_, index) => (
+        <FaStar key={index} color="#F8B73B" />
+      ))}
+    </div>
+    <span className='text-sm text-[#3A98BB] tracking-[0.33px] leading-[18px]'>(5 Reviews)</span>
+    </div>
+      <div>
+        <span className='text-[#767676] tracking-[0.33px] leading-[18px]'>Member since 12, June 2024</span>
+    </div>
+    </div>
+    <div className='flex flex-col gap-2 '>
+        <div className='flex items-center gap-2 text-[#76767676] '>
+            <span className='font-bold text-[#222222]'>14</span>
+             <span>Jobs Posted</span>
         </div>
-        <div className='flex gap-2 mt-2 items-center'>
-        <p className='text-sm text-[#767676]'>Ratings</p>
-            <Image src={"/dev-images/rating.png"} width={40} height={40} alt='location' />
-            <p className='text-sm text-[#3A98BB]'>(5 Reviews)</p>
-        </div>
-        <div className='flex gap-2 mt-2 items-center'>
-        <p className='text-sm text-[#767676]'>Members since 12 June, 2024</p>
-        </div>
-        <div className='flex gap-2 items-center mt-8'>
-        <p className='text-base font-bold text-[#222222]'>14</p>
-            <p className='text-sm text-[#767676]'>Jobs Posted</p>
-        </div>
-        <div className='flex gap-2 items-center mt-2'>
-        <p className='text-base font-bold text-[#222222]'>14</p>
-            <p className='text-sm text-[#767676]'>Artists Hired</p>
-        </div>
-        <div className='mt-8'>
-        <h6 className='font-bold text-[#222222]'>Client Verifications</h6>
-        </div>
-        <div className='flex gap-2 mt-4 items-center'>
-            <Image src={"/dev-images/icon1.png"} width={20} height={20} alt='location' />
-            <p className='text-sm text-[#767676]'>Identity Verified</p>
-        </div>
-        <div className='flex gap-2 mt-4 items-center'>
-            <Image src={"/dev-images/Email.png"} width={20} height={20} alt='location' />
-            <p className='text-sm text-[#767676]'>Email Address Verified</p>
-        </div>
-        <div className='flex gap-2 mt-4 items-center'>
-            <Image src={"/dev-images/CreditCard.png"} width={20} height={20} alt='location' />
-            <p className='text-sm text-[#767676]'>Payment Method Verified</p>
+        <div className='flex items-center gap-2 text-[#76767676] '>
+            <span className='font-bold text-[#222222]'>14</span>
+             <span>Artist Hired</span>
         </div>
     </div>
     </div>
-    </CardBody>
-  </Card>
+    <div>
+       <div className='text-lg mb-[22px]'>
+        <h5 className='leading-[160%] tracking-[0.33px]'>Client Verifications</h5>
+    </div>
+    <div className='flex flex-col gap-4'>
+    <div className='flex items-center gap-2'>
+     <FaRegUser color='#3A98BB' />
+     <span>Identity Verified</span>
+    </div>
+    <div className='flex items-center gap-2'>
+     <MdOutlineMailOutline color='#3A98BB' />
+     <span>Email Address Verified</span>
+    </div>
+     <div className='flex items-center gap-2'>
+     <MdOutlinePhone color='#3A98BB' />
+     <span>Phone Number Verified</span>
+    </div>
+    <div className='flex items-center gap-2'>
+     <LuCreditCard color='#3A98BB' />
+     <span>Payment Method Verified</span>
+    </div>
+    </div>
+    </div>
+  </div>
   )
 }
 
