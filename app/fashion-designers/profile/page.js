c2b9b1cce801e3f75eb813e41d5eb028ebbebf5a
Divine Etidy <divinetidy@gmail.com>
2025-07-26 21:51:38 +0100

    worked on profile page

--- app/fashion-designers/profile/page.js
+++ app/fashion-designers/profile/page.js
@@ -33,7 +33,7 @@ const reviews = [
 const fullText = `Sorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum tellus. Curabitur tempor quis eros tempus lacinia. Nam bibendum pellentesque quam a convallis. Sed ut vulputate nisi. Integer in felis sed leo vestibulum venenatis. Suspendisse quis arcu sem. Aenean feugiat ex eu vestibulum vestibulum. Morbi a eleifend magna. Nam metus lacus, porttitor eu mauris a, blandit ultrices nibh. Mauris sit amet magna non ligula vestibul`;
 
 const Page = () => {
-  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+  const [isMobile, setIsMobile] = useState(false);
   const [showFull, setShowFull] = useState(false);
   const [aboutValue, setAboutValue] = useState(fullText);
   const [titleValue, setTitleValue] = useState(
@@ -83,7 +83,6 @@ const Page = () => {
   const displayedText =
     isMobile && !showFull ? words.slice(0, 30).join(" ") + "..." : fullText;
 
-    
   return (
     <div className=" md:bg-[#F1F1F1] md:px-12">
       {/* Header */}
