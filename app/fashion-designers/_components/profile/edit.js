f7b4c899ae3782f266782ece04644caf7c1740f9
Enang <divinetidy@gmail.com>
2025-07-29 20:03:49 +0100

    Divine/profile (#240)
    * worked on profile page

---
+++ app/fashion-designers/_components/profile/edit.js
@@ -0,0 +1,206 @@
+import Image from "next/image";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+  Button,
+} from "@heroui/react";
+import { useState } from "react";
+
+// This component handle edit title button
+
+export const EditTitle = ({ setTitleValue, titleValue }) => {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+  return (
+    <>
+      <button
+        className="bg-[radial-gradient(circle,#FFFFFF,#CCE7F2)] flex items-center gap-1 rounded-3xl drop-shadow-lg p-2 md:py-2 md:px-4"
+        onClick={onOpen}
+      >
+        <Image
+          src="\profile\pencil.svg"
+          alt="icon"
+          width={20}
+          height={20}
+          className=""
+        />
+        <span className="hidden md:block font-satoshi font-bold text-base text-[#035A7A]">
+          Edit
+        </span>
+      </button>
+      <Modal
+        isOpen={isOpen}
+        placement="center"
+        onOpenChange={onOpenChange}
+        shouldcloseonoutsideclick="false"
+        size="4xl"
+        className="w-3/4 md:w-2/4 md:p-8"
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalBody>
+                <div className="flex flex-col ">
+                  <h1 className="text-[#222222] font-bold text-lg md:text-xl font-satoshi">
+                    TITLE
+                  </h1>
+
+                  <input
+                    value={titleValue}
+                    onChange={(e) => setTitleValue(e.target.value)}
+                    className="border-[#D5D5D5] mt-2 outline-none text-base text-[#222222] p-3 h-12 w-full border-1 rounded-lg"
+                  />
+                  <div className="flex items-center justify-end gap-1 mt-10 font-bold font-satoshi text-base">
+                    <button
+                      onClick={onClose}
+                      className="py-2 px-6 text-[#444444]  rounded-3xl bg-[#E8E8E8] outline-0 "
+                    >
+                      Cancel
+                    </button>
+                    <button className="bg-[radial-gradient(circle,#FFFFFF,#CCE7F2)] text-[#035A7A] py-2 px-6 rounded-3xl">
+                      Save
+                    </button>
+                  </div>
+                </div>
+              </ModalBody>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+// This component handle edit about me button
+
+export const EditAboutMe = ({ setAboutValue, aboutValue }) => {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+  return (
+    <>
+      <button
+        className="bg-[radial-gradient(circle,#FFFFFF,#CCE7F2)] flex items-center gap-1 rounded-3xl drop-shadow-lg p-2 md:py-2 md:px-4"
+        onClick={onOpen}
+      >
+        <Image
+          src="\profile\pencil.svg"
+          alt="icon"
+          width={20}
+          height={20}
+          className=""
+        />
+        <span className="hidden md:block font-satoshi font-bold text-base text-[#035A7A]">
+          Edit
+        </span>
+      </button>
+      <Modal
+        isOpen={isOpen}
+        placement="center"
+        onOpenChange={onOpenChange}
+        shouldcloseonoutsideclick="false"
+        size="5xl"
+        className="w-3/4 md:w-2/3  md:p-8"
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalBody>
+                <div className="flex flex-col ">
+                  <h1 className="text-[#222222] font-bold text-lg md:text-xl font-satoshi">
+                    ABOUT ME
+                  </h1>
+
+                  <textarea
+                    value={aboutValue}
+                    onChange={(e) => setAboutValue(e.target.value)}
+                    className="border-[#D5D5D5] text-[#222222] mt-2 outline-none text-base p-1 md:p-3 h-52 w-full border-1 rounded-lg"
+                  />
+                  <div className="flex items-center justify-end gap-1 mt-10 font-bold font-satoshi text-base">
+                    <button
+                      onClick={onClose}
+                      className="py-2 px-6 text-[#444444]  rounded-3xl bg-[#E8E8E8] outline-0 "
+                    >
+                      Cancel
+                    </button>
+                    <button className="bg-[radial-gradient(circle,#FFFFFF,#CCE7F2)] text-[#035A7A] py-2 px-6 rounded-3xl">
+                      Save
+                    </button>
+                  </div>
+                </div>
+              </ModalBody>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+// This component handle edit award button
+
+export const EditAward = ({ handleAwardChange }) => {
+  return (
+    <>
+      {" "}
+      <input
+        type="file"
+        accept="image/*"
+        id="upload-award"
+        className="hidden"
+        onChange={handleAwardChange}
+      />
+      <label
+        htmlFor="upload-award"
+        className="bg-[radial-gradient(circle,#FFFFFF,#CCE7F2)] flex items-center gap-1 p-2 z-20 cursor-pointer rounded-full"
+      >
+        <Image
+          src="\profile\pencil.svg"
+          alt="icon"
+          width={20}
+          height={20}
+          className=" md:hidden"
+        />
+        <Image
+          src="\profile\plus.svg"
+          alt="icon"
+          width={20}
+          height={20}
+          className="hidden md:block"
+        />
+        <p className="text-[#035A7A] font-bold text-base">Add</p>
+      </label>
+    </>
+  );
+};
+
+// This component handle edit profile button
+
+export const EditProfile = ({ handleFileChange }) => {
+  return (
+    <>
+      {" "}
+      <input
+        type="file"
+        accept="image/*"
+        id="upload-photo"
+        className="hidden"
+        onChange={handleFileChange}
+      />
+      <label
+        htmlFor="upload-photo"
+        className="bg-[radial-gradient(circle,#FFFFFF,#CCE7F2)] p-2 z-20 cursor-pointer rounded-full absolute"
+      >
+        <Image
+          src="\profile\pencil.svg"
+          alt="icon"
+          width={20}
+          height={20}
+          className=" "
+        />
+      </label>
+    </>
+  );
+};
