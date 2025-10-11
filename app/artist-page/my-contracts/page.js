f31b5b673e905663c8005dfa22dc2339439ce47e
aorfdesigns <rahformatives@gmail.com>
2025-10-11 16:23:49 +0100

    resolved conflicts

--- app/artist-page/my-contracts/page.js
+++ app/artist-page/my-contracts/page.js
@@ -5,39 +5,3 @@ import ContractPage from "./components/contracts";
 export default function Page() {
   return <ContractPage />;
 }
-
-import React from "react";
-import UnderlinedTabs from "./_components/Tabs";
-import SearchBar from "../../../components/Searchbar";
-import PaginationTab from "../../../components/Pagination";
-import { ChevronLeft } from "lucide-react";
-
-const Page = () => {
-  return (
-    <>
-      <div>
-        {/* Title Row */}
-        <div className="flex items-center ml-8 mt-8 mb-2">
-          {/* Show Chevron only on mobile */}
-          <button className="lg:hidden mr-2">
-  <ChevronLeft className="w-6 h-6 text-[#878787]" />
-</button>
-
-
-          <h1 className="lg:text-2xl text-[20px] font-bold text-[#222222]">
-            My Contracts
-          </h1>
-        </div>
-
-        <UnderlinedTabs />
-      </div>
-
-      {/* Center Pagination */}
-      <div className="flex justify-center">
-        <PaginationTab />
-      </div>
-    </>
-  );
-};
-
-export default Page;
