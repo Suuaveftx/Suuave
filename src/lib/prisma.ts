d9954e6cdbdc87757e4510e5792933f524fb42cb
lamodots <lamodots@gmail.com>
2025-12-16 17:17:58 -0800

    try psima fi 11

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,8 @@
 // import { PrismaClient } from '../generated/prisma/client.js';
 
-import { PrismaClient } from '@/generated/prisma/client.js';
+// import { PrismaClient } from '@/generated/prisma/client.js';
 import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@prisma/client';
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL!,
