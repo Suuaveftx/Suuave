b92f654a297643c1c240483310ab0abd15592165
lamodots <lamodots@gmail.com>
2025-12-16 17:46:43 -0800

    try psima fi 15

--- app/join-waitlist/page.tsx
+++ app/join-waitlist/page.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import HeroSection from './_components/Hero/Horo';
 import CustomNavbar from '../../components/Navbar';
 import Footer from '../about-page/components/Footer';
-
 function page() {
   return (
     <div className='max-w-[1700px] mx-auto bg-customNavBg'>
