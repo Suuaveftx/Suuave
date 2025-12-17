8b7f526a1a09d486819643e6ddc33f5701c7c3b8
lamodots <lamodots@gmail.com>
2025-12-16 16:50:03 -0800

    try psima fi 10

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,6 @@
 // import { PrismaClient } from '../generated/prisma/client.js';
 
-import { PrismaClient } from '@/generated/prisma/client';
+import { PrismaClient } from '@/generated/prisma/client.js';
 import { PrismaPg } from '@prisma/adapter-pg';
 
 const adapter = new PrismaPg({
