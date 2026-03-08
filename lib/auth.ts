edf5574939536144ade20039205e48e77ee15e7b
lamodots <lamodots@gmail.com>
2026-03-08 08:57:51 +0000

    sync with new shared prisma

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,6 @@
 import { betterAuth } from 'better-auth';
-import { prismaAdapter } from 'better-auth/adapters/prisma';
+// import { prismaAdapter } from 'better-auth/adapters/prisma';
+import { prismaAdapter } from '@better-auth/prisma-adapter';
 
 import { nextCookies } from 'better-auth/next-js';
 import { emailOTP } from 'better-auth/plugins';
